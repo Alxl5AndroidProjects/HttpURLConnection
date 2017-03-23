f2a9ae9d2caa771c6e4e1dc3422765c06e6d4aaf
AlxL5 <alexandr.minenko@gmail.com>
2017-03-23 23:34:42 +0700

    Add JSONTask class

--- app/src/main/java/com/alxl5/asynctaskproject/MainActivity.java
+++ app/src/main/java/com/alxl5/asynctaskproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.alxl5.asynctaskproject;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     public Button button;
     public TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +23,21 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView.setText("Ok! Button Click!");
+                JSONTask jsonTask = new JSONTask();
+                jsonTask.execute("Ok! Add JSONTask class!", null, null);
             }
         });
+    }
+
+    private class JSONTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String[] params) {
+            return params[0];
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
     }
 }
