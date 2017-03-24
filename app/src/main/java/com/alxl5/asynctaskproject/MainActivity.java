a624c4aa30325c4714463e52d65f98ec003e9a3d
AlxL5 <alexandr.minenko@gmail.com>
2017-03-24 21:59:58 +0700

    Add Listview, adapter...

--- app/src/main/java/com/alxl5/asynctaskproject/MainActivity.java
+++ app/src/main/java/com/alxl5/asynctaskproject/MainActivity.java
@@ -1,11 +1,22 @@
 package com.alxl5.asynctaskproject;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.alxl5.asynctaskproject.models.BlogModel;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,11 +29,13 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    TextView textView;
+    ListView blogListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
-        textView = (TextView) findViewById(R.id.textView);
+
+        blogListView = (ListView) findViewById(R.id.blogListView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,9 +55,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class JSONTask extends AsyncTask<String, String, String> {
+    private class JSONTask extends AsyncTask<String, String, List<BlogModel>> {
         @Override
-        protected String doInBackground(String[] params) {
+        protected List<BlogModel> doInBackground(String[] params) {
 
             HttpURLConnection connection = null;
             BufferedReader reader = null;
@@ -54,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
                 connection.setRequestMethod("POST");
                 connection.setDoOutput(true);
                 connection.setDoInput(true);
-                //connection.setRequestProperty("Content-Type", "application/json");
                 connection.setConnectTimeout(1000);
                 connection.setReadTimeout(1000);
 
@@ -79,16 +92,24 @@ public class MainActivity extends AppCompatActivity {
 
                 StringBuffer stringBufferData = new StringBuffer();
 
-                for(int i = 0; i <jsonArray.length(); i++) {
+                List<BlogModel> blogModelList = new ArrayList<>();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
 
                     JSONObject resultObject = jsonArray.getJSONObject(i);
+                    BlogModel blogModel = new BlogModel();
+                    blogModel.setId(resultObject.getInt("id"));
+                    blogModel.setTitle(resultObject.getString("title"));
+                    blogModel.setText(resultObject.getString("text"));
+                    blogModel.setCreate_date(resultObject.getString("create_date"));
 
                     String id = resultObject.getString("id");
                     stringBufferData.append("ID: " + id + "\n");
 
+                    blogModelList.add(blogModel);
                 }
 
-                return stringBufferData.toString();
+                return blogModelList;
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -105,9 +126,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(String result) {
+        protected void onPostExecute(List<BlogModel> result) {
             super.onPostExecute(result);
-            textView.setText(result);
+            BlogAdapter adapter = new BlogAdapter(getApplicationContext(), R.layout.row, result);
+            blogListView.setAdapter(adapter);
+        }
+    }
+
+    public class BlogAdapter extends ArrayAdapter {
+
+        private List<BlogModel> blogModelList;
+        private int resourse;
+        private LayoutInflater inflater;
+
+        public BlogAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List objects) {
+            super(context, resource, objects);
+            blogModelList = objects;
+            this.resourse = resource;
+            inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.row, null);
+            }
+
+            TextView titleView;
+            TextView textView;
+            TextView dateView;
+
+            titleView = (TextView) convertView.findViewById(R.id.titleView);
+            textView = (TextView) convertView.findViewById(R.id.textView);
+            dateView = (TextView) convertView.findViewById(R.id.dateView);
+
+            titleView.setText(blogModelList.get(position).getTitle());
+            textView.setText(Html.fromHtml(blogModelList.get(position).getText()));
+            dateView.setText(blogModelList.get(position).getCreate_date());
+
+            return convertView;
         }
     }
 }
---
+++ app/src/main/java/com/alxl5/asynctaskproject/models/BlogModel.java
@@ -0,0 +1,46 @@
+package com.alxl5.asynctaskproject.models;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class BlogModel {
+
+    private int id;
+    private String title;
+    private String text;
+    private String create_date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(String create_date) {
+        this.create_date = create_date;
+    }
+
+}
