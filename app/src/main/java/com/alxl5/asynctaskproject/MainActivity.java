5bb4085cfbb402e261f0500d3b2df74954128792
AlxL5 <alexandr.minenko@gmail.com>
2017-03-24 14:19:42 +0700

    Change files

--- app/src/main/java/com/alxl5/asynctaskproject/MainActivity.java
+++ app/src/main/java/com/alxl5/asynctaskproject/MainActivity.java
@@ -7,10 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
-    public Button button;
-    public TextView textView;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 JSONTask jsonTask = new JSONTask();
-                jsonTask.execute("Ok! Add JSONTask class!", null, null);
+                jsonTask.execute("http://alxl5-domain.esy.es/index.php/ajax/response", null, null);
             }
         });
     }
@@ -32,11 +40,51 @@ public class MainActivity extends AppCompatActivity {
     private class JSONTask extends AsyncTask<String, String, String> {
         @Override
         protected String doInBackground(String[] params) {
-            return params[0];
+
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                //connection.setRequestProperty("Content-Type", "application/json");
+                connection.setConnectTimeout(1000);
+                connection.setReadTimeout(1000);
+
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder sb = new StringBuilder();
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                return sb.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                assert connection != null;
+                connection.disconnect();
+            }
+
+            return null;
         }
 
         @Override
         protected void onPostExecute(String result) {
+            super.onPostExecute(result);
             textView.setText(result);
         }
     }
